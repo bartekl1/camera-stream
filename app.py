cec031fa942e04f5065927db29b47c08a676043c
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-04-01 20:07:36 +0200

    Streaming improved

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
 from flask import Flask, render_template, Response
 import cv2
 
+import threading
+import time
+
 app = Flask(__name__)
-#app.config["CACHE_TYPE"] = "null"
+
+camera_frame = None
+
+
+def generate_frames():
+    global camera_frame
+    print("Thread started")
+    vs = cv2.VideoCapture(0)
+    while True:
+        ret, frame = vs.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        camera_frame = (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        time.sleep(0.1)
+
+
+def get_frame():
+    while camera_frame is None:
+        time.sleep(0.1)
+    while True:
+        yield camera_frame
+        time.sleep(0.1)
+
+
+@app.before_first_request
+def start_thread():
+    generator_thread = threading.Thread(target=generate_frames)
+    generator_thread.start()
+
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
-def gen():
-    """Video streaming generator function."""
-    vs = cv2.VideoCapture(0)
-    while True:
-        ret,frame=vs.read()
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        frame=jpeg.tobytes()
-        yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
-    vs.release()
-    cv2.destroyAllWindows() 
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(get_frame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__': 
-    app.run(host='0.0.0.0', port =5000, debug=True, threaded=True)
-    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
